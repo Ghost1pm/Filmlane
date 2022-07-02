e337faefb1c45de99ce624679868d8bc79a23d2e
Shinchan54 <91662751+Shinchan54@users.noreply.github.com>
2022-07-02 14:17:18 +0530

    Filmlane

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$('#sliderbar').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+          items:1
+        },
+        550:{
+            items:2
+        },
+        991:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
